fb53d663e0e9a5953f636ea2261aa63089e360e3
juan lentino <nuncaparesdecrear@gmail.com>
2022-08-22 18:52:22 -0500

    MediaImg test

---
+++ src/MediaImg/MediaImg.spec.js
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react"
+import MediaImg from "./MediaImg"
+
+describe('@components/MediaImg', ()=> {
+    it('mensaje de img', ()=> {
+        // arrange 
+        const { getByRole } = render(
+            <MediaImg  alt="img" src="https://www.algo.com/img" />
+        )
+        
+        // act 
+        const MediaImgTest = getByRole('img')
+
+        // assert 
+        expect(MediaImgTest).toBeInTheDocument();
+        expect(MediaImgTest).toHaveAttribute('src', 'https://www.algo.com/img')
+        expect(MediaImgTest).toHaveAttribute('alt', 'img')
+    })
+})
